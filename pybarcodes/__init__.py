f0ee0fcfe294524817464b5c7287271dbac02bf8
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-05-15 00:55:42 +0300

    forgot to bump up version

--- pybarcodes/__init__.py
+++ pybarcodes/__init__.py
@@ -12,7 +12,7 @@ __title__ = "pybarcodes"
 __author__ = "Vitaman02"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present Vitaman02"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 from collections import namedtuple
