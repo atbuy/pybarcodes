0476c901b3d6c29e7978539fc4c4ff8b580d10bf
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-05-15 11:04:28 +0300

    bumped up version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Vitaman02",
     url="https://github.com/Vitaman02/pybarcodes",
     project_urls={},
-    version="0.4.5",
+    version="0.4.6",
     packages=find_packages(),
     license="MIT",
     description="A Python barcode generator",
