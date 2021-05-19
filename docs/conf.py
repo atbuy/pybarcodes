a18e6c4c27b7235c8f5f6817a25a00754473c1f7
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-05-19 12:16:00 +0300

    implemented code39 barcodes

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Vitaman02'
 author = 'Vitaman02'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.5'
+release = '0.7.2'
 
 
 # -- General configuration ---------------------------------------------------
