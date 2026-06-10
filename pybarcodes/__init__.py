6c3a51d166188a48865a218efc065d1accca56aa
atbuy <buy@atbuy.dev>
2026-06-10 18:44:36 +0300

    Updated vulnerable packages

--- pybarcodes/__init__.py
+++ pybarcodes/__init__.py
@@ -1,16 +1,18 @@
 from collections import namedtuple
 
-from pybarcodes.codes import CODE39, Code  # noqa: F401
-from pybarcodes.ean import EAN, EAN8, EAN13, EAN14, JAN, Size, Weights  # noqa: F401
+from pybarcodes.codes import CODE39, Code
+from pybarcodes.ean import EAN, EAN8, EAN13, EAN14, JAN, Size, Weights
 
 __title__ = "pybarcodes"
 __author__ = "atbuy"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present atbuy"
-__version__ = (1, 2, 2)
+__version__ = (1, 2, 3)
 
 VersionInfo = namedtuple("VersionInfo", "major minor patch")
 
 version_info = VersionInfo(*__version__)
 
 SUPPORTED_BARCODES = ["EAN13", "EAN8", "EAN14", "JAN", "CODE39"]
+
+__all__ = ("CODE39", "Code", "EAN", "EAN8", "EAN13", "EAN14", "JAN", "Size", "Weights")
